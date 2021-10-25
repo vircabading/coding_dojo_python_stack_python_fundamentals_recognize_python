557938a767e113f47f335e9be3cc7ba3da14cd9f
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-10-24 19:30:53 -0700

    add for loop comments

--- file.py
+++ file.py
@@ -20,24 +20,28 @@ person['name'] = 'George'                       # change value of dictionary com
 person['eye_color'] = 'blue'                    # initialize value of dictionary composite data type using a key
 print(fruit[2])                                 # access value of tuple composite data type using an index
 
-if num1 > 45:
-    print("It's greater")
-else:
-    print("It's lower")
+if num1 > 45:                                   # if conditional, access value of number primitive data type
+    print("It's greater")                       # function that prints a string
+else:                                           # else conditional
+    print("It's lower")                         # function that prints a string
 
-if len(string) < 5:
-    print("It's a short word!")
-elif len(string) > 15:
+if len(string) < 5:                             # if conditional, len funtion, returns the number of characters in a string
+                                                # access value of a string primitive data type
+    print("It's a short word!")                 # function that prints a string
+elif len(string) > 15:                          # else if contditional (note elif is short hand for else if)
+                                                # len function, retrurns the number of characters if a string is passed as a parameter
     print("It's a long word!")
-else:
+else:                                           # else conditional
     print("Just right!")
 
-for x in range(5):
-    print(x)
-for x in range(2,5):
-    print(x)
-for x in range(2,10,3):
-    print(x)
+for x in range(5):                              # for loop, start at 0, stop at 6, increment by 1
+                                                # range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and ends at a specified number.
+    print(x)                                    # prints a number
+for x in range(2,5):                            # for loop, start at 2, stop at 5, increment by 1
+    print(x)                                    # prints a number
+for x in range(2,10,3):                         # for loop, start at 2, stop at 10, increment by 3
+    print(x)                                    # prints a number
+    
 x = 0
 while(x < 5):
     print(x)
